d01b33df12e974c214dc6c7c8308b78c759012f4
unarunars <unr2@hi.is>
2020-05-24 21:55:58 +0000

    smá

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,21 +9,7 @@ import {MatButtonModule} from '@angular/material/button';
 import { PaintingsComponent } from './paintings/paintings.component';
 import { DesignComponent } from './design/design.component';
 import {MatGridListModule} from '@angular/material/grid-list';
-import { LightboxModule } from 'ngx-lightbox';
 
-/* Custom Hammer configuration */
-import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
-import * as Hammer from 'hammerjs';
-
-export class CustomHammerConfig extends HammerGestureConfig {
-  overrides = {
-    'pan': {
-      direction: Hammer.DIRECTION_ALL,
-    }
-  }
-}
-/* End Custom hammer configuration */
-//import { NgxGalleryModule } from 'ngx-gallery';
 
 
 @NgModule({
@@ -39,13 +25,9 @@ export class CustomHammerConfig extends HammerGestureConfig {
     BrowserAnimationsModule,
     MatButtonModule,
     MatGridListModule,
-    LightboxModule,
-    //GalleryModule,
-    //NgxGalleryModule
     
   ],
   providers: [
-    {provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig}
   ],
   bootstrap: [AppComponent]
 })
